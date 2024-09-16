750e4658513c54df4e5ca0c5cd898433c51f4e70
mixei1902 <misei190289@gmail.com>
2024-09-16 17:54:16 +0300

    add CRUD

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,1 +1,34 @@
 # Логика работы с пользователями (CRUD операции, бизнес-логика)
+from tortoise.exceptions import DoesNotExist
+
+from app.db.models import User
+from app.schemas.user_schema import CreateUser, UpdateUser
+
+
+async def get_user_by_id(user_id: int):
+    try:
+        return await User.get(id=user_id)
+    except DoesNotExist:
+        return None
+
+
+async def create_user(user_data: CreateUser):
+    user = await User.create(**user_data.dict())
+    return user
+
+
+async def update_user(user_id: int, user_data: UpdateUser):
+    user = await get_user_by_id(user_id)
+    if not user:
+        return None
+    await user.update_from_dict(user_data.dict(exclude_unset=True))
+    await user.save()
+    return user
+
+
+async def delete_user(user_id: int):
+    user = await get_user_by_id(user_id)
+    if user:
+        await user.delete()
+        return True
+    return False
