0a10ab3a079ee580d4a1c1ea07f336831265e8be
mixei1902 <misei190289@gmail.com>
2024-09-16 17:53:23 +0300

    add schema (validations)

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,1 +1,33 @@
 # Pydantic-модели для валидации данных пользователей
+from datetime import date
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr
+
+
+class UserBase(BaseModel):
+    first_name: str
+    last_name: str
+    other_name: Optional[str] = None
+    email: EmailStr
+    phone: Optional[str] = None
+    birthday: Optional[date] = None
+
+
+class CreateUser(UserBase):
+    password: str
+    city: Optional[int] = None
+    additional_info: Optional[str] = None
+
+
+class UpdateUser(UserBase):
+    city: Optional[int] = None
+    additional_info: Optional[str] = None
+
+
+class UserResponse(UserBase):
+    id: int
+    is_admin: bool
+
+    class Config:
+        orm_mode = True
