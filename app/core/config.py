4c0f5b38a02f625451f945eeb27f3d0961d022cf
mixei1902 <misei190289@gmail.com>
2024-09-16 17:55:54 +0300

    add JWT + config

--- app/core/config.py
+++ app/core/config.py
@@ -1,1 +1,15 @@
 # Основная конфигурация приложения (переменные среды, настройки базы данных)
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    database_url: str
+    secret_key: str = "your_secret_key"
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
--- app/db/models.py
+++ app/db/models.py
@@ -7,15 +7,16 @@ class User(Model):
     id = fields.IntField(pk=True)
     first_name = fields.CharField(max_length=50)
     last_name = fields.CharField(max_length=50)
-    email = fields.CharField(max_length=100, unique=True)
-    password_hash = fields.CharField(max_length=128)
+    other_name = fields.CharField(max_length=50, null=True)
+    email = fields.CharField(max_length=255, unique=True)
+    phone = fields.CharField(max_length=20, null=True)
+    birthday = fields.DateField(null=True)
     is_admin = fields.BooleanField(default=False)
-
-    def full_name(self):
-        return f"{self.first_name} {self.last_name}"
+    city = fields.IntField(null=True)
+    additional_info = fields.TextField(null=True)
 
     class Meta:
         table = "users"
 
     def __str__(self):
-        return self.full_name()
+        return f"{self.first_name} {self.last_name}"
