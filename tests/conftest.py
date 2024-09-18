d6c8a9849bd9d7e0a75df3fd71a9a3b23ab3e8a2
mixei1902 <misei190289@gmail.com>
2024-09-18 17:32:32 +0300

    fix test

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,15 +10,17 @@ from app.main import app
 
 @pytest.fixture(scope="session")
 def event_loop():
-    return asyncio.get_event_loop()
-
+    # Используем WindowsSelectorEventLoopPolicy для совместимости с Windows
+    if asyncio.get_event_loop_policy().__class__.__name__ != 'WindowsSelectorEventLoopPolicy':
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
 
 @pytest.fixture(scope="module", autouse=True)
-async def initialize_tests():
-    # Инициализируем базу данных для тестирования
-    initializer(['app.db.models'], db_url=settings.test_database_url, app_label='models')
+def initialize_tests(event_loop):
+    initializer(['app.db.models'], db_url='postgres://postgres:12345@localhost:5432/TA6_test', app_label='models')
     yield
-    # Завершаем работу с базой данных после тестов
     finalizer()
 
 
